fe085338a8a47b8a19ef281a2bd79bb311978565
Mateus Santos <mateussilvanzg@gmail.com>
2024-08-02 21:56:42 -0300

    update media query 320-450

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 function toggleMode() {
   const mobile_menu = document.getElementById("mobile_menu");
-  const mobile_icon = document.getElementById("mobile_icon");
 
   mobile_menu.classList.toggle("active");
 }
